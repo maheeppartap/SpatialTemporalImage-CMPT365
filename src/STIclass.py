gui fixes, added a structure, kind of works. added normalization.

src/STIclass.py
@@ -4,12 +4,12 @@ import traceback
 
 class STI:
     def __init__(self, rows, cols, x=3):
-        self.sti = np.array([[[float(0),float(0),float(0)] for i in range(int(rows))] for j in range(int(cols))])
+        self.sti = np.array([[[float(0),float(0),float(0)] for i in range(int(cols))] for j in range(int(rows))])
 
     # adds a column at pos index for col STI. Default way.
     def addCol(self, pos, col ):
         try:
-            self.sti[pos] = col
+            self.sti[:,pos] = col[:]
         except ValueError:
             print("Invalid column input.")
             traceback.print_exc()
@@ -21,7 +21,7 @@ class STI:
     # adds a row for row STI. Could use, but meh.
     def addRow(self, pos, row):
         try:
-            self.sti[pos, :] = row
+            self.sti[pos] = row
         except ValueError:
             print("Invalid row input.")
             traceback.print_exc()
e206130aaeadb2298ef8f39a6109b7705cf1f8f6
maheep <maheeppartap47@gmail.com>
2020-03-18 22:14:18 -0700

    gui fixes, added a structure, kind of works. added normalization.

--- src/STIclass.py
+++ src/STIclass.py
@@ -4,12 +4,12 @@ import traceback
 
 class STI:
     def __init__(self, rows, cols, x=3):
-        self.sti = np.array([[[float(0),float(0),float(0)] for i in range(int(rows))] for j in range(int(cols))])
+        self.sti = np.array([[[float(0),float(0),float(0)] for i in range(int(cols))] for j in range(int(rows))])
 
     # adds a column at pos index for col STI. Default way.
     def addCol(self, pos, col ):
         try:
-            self.sti[pos] = col
+            self.sti[:,pos] = col[:]
         except ValueError:
             print("Invalid column input.")
             traceback.print_exc()
@@ -21,7 +21,7 @@ class STI:
     # adds a row for row STI. Could use, but meh.
     def addRow(self, pos, row):
         try:
-            self.sti[pos, :] = row
+            self.sti[pos] = row
         except ValueError:
             print("Invalid row input.")
             traceback.print_exc()
--- src/guiCode.py
+++ src/guiCode.py
@@ -1,31 +1,42 @@
 import tkinter as tk
 from tkinter import filedialog
+
 import cv2
-import numpy as np
-import png
-import matplotlib
-import PIL
+
 from STIclass import STI
-from tkinter import messagebox
-from PIL import Image
-from sklearn.preprocessing import normalize
+import numpy as np
 
 global tink  # the parent declaration
 global fileName
 global sti
-
+global videoPlayer
+global frameBG
 
 def begin():
     global tink
+    global videoPlayer
+    global frameBG
 
     tink = tk.Tk()  # the parent window
-    tink.geometry("500x400")
-    tink.title("test")
-    # add widgets here
-    chooserButton = tk.Button(tink, text="pick a file", width=25, command=fileselection)
 
+    appRes = [500, 500] #for a 500x500 res
+
+
+    tink.title("test")
+    tink.geometry("%dx%d" % (appRes[0],appRes[1]))
+    bgName = tk.PhotoImage(file="../assets/bg.png")
+    frameBG = tk.Frame(tink, bg="white")
+    frameBG.place(relx=0,rely=0,relwidth=1,relheight=1)
+    backgroundIMG=tk.Label(frameBG, image=bgName)
+    backgroundIMG.place(relx=0,rely=0,relwidth=1,relheight=1)
+
+    # add widgets here
+    chooserButton = tk.Button(frameBG, text="Pick a file", width=25, command=fileselection, relief="ridge")
+    videoPlayer = tk.Label(frameBG,bg="black")
     # this is for arrangement of widgets
-    chooserButton.pack()
+
+    chooserButton.place(relx=0.4,rely=0.75,relwidth=0.2,relheight=0.07)
+    videoPlayer.place(relx=0.15,rely=0.2,relwidth=0.7,relheight=0.5)
     tink.protocol("WM_DELETE_WINDOW", onClosing)
     w = tink.mainloop()
 
@@ -40,31 +51,38 @@ def fileselection():
 
 
 def videoBreakDown():
+    global sti
+    global videoPlayer
+    global frameBG
     windowname = "CMPT 365 Project"
     vidCapture = cv2.VideoCapture(fileName)
     if not vidCapture.isOpened():
         print("Error opening the stream")
 
     successful, image = vidCapture.read()
-    global sti
+
     width = vidCapture.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = vidCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)
     length = int(vidCapture.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = vidCapture.get(cv2.CAP_PROP_FPS)
     currFrame = 0
     print("video is: ", width, " ", height)
+    print("Number of frames: ", length)
     sti = STI(width, length)
     cv2.namedWindow(windowname)
     cv2.startWindowThread()
 
     middleCol = int(width / 2)
 
-
+#########################################working here 18 march
     while currFrame < length-1:
         sti.addCol(currFrame, image[middleCol])
-        #print("Sending colour: ", image[middleCol], "Done")
-        successful, image = vidCapture.read()
-        cv2.imshow(windowname, image)
+        image_ = tk.PhotoImage(vidCapture.read()[1].all())#vidCapture.read()
+        x = image_
+        videoPlayer=tk.Label(frameBG,image=x)
+        videoPlayer.image = x
+        videoPlayer.place(relx=0.15, rely=0.2, relwidth=0.7, relheight=0.5)
+        #cv2.imshow(windowname, image_)
         if cv2.waitKey(int(1000 / fps)) & 0xFF == ord('q'):
             break
         if cv2.getWindowProperty(windowname, 0) < 0:
@@ -72,8 +90,8 @@ def videoBreakDown():
         currFrame += 1
    # print(width)
 
-    for i in range(int(length)):
-        for j in range(int(width)):
+    for i in range(int(width)):
+        for j in range(int(length)):
             for k in range(3):
                 #print(i," " ,j , " ", k)
                 sti.sti[i][j][k] /= 255
@@ -82,9 +100,7 @@ def videoBreakDown():
     cv2.waitKey(25000)
     vidCapture.release()
     cv2.destroyAllWindows()  # just to be safe
-
-def normme(m, width, height):
-    pass
+    onClosing()
 
 def onClosing():
     tink.destroy()
