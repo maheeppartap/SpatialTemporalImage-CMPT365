5bd84334921e71f39c92398bbeade0d53b3a07d2
maheep <maheeppartap47@gmail.com>
2020-04-12 18:02:48 -0700

    deployment works

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
 from setuptools import setup
 
 setup(
-    name="SpatialTemporalImage-CMPT365",
+    name="stiDetection",
     version="1.0",
     description="Generates and analyzes STIs for videos and creates"
                 "a enhanced video.",
     author="Maheeppartap Singh""Conor Murphy",
-    packages=['src'],
+    packages=['src'
+               ],
     install_requires=[
         'opencv-python',
         'matplotlib',
         'numpy'
-    ]
+    ],
+    scripts=['src/CLI.py',
+             'src/transitionDetector.py',
+             'src/videoEnhancer.py',
+             'src/transitions.py',
+             'src/videoAnalysis.py',
+             'src/videoBreakdown.py',
+             'src/videoSpecs.py'
+             ],
+
+    entry_points={
+      'console_scripts': [
+          'enhanceMe = CLI:main'
+      ]
+    }
 )
+
