02694b8f048b22d950f8d03ce1382123edaf83d1
conor <conormurphyx@gmail.com>
2020-04-13 00:42:03 -0400

    added hough and resolution option

--- src/CLI.py
+++ src/CLI.py
@@ -7,7 +7,6 @@ import cv2
 from videoEnhancer import enhance
 from videoBreakdown import breakdowntoSTI
 from transitionDetector import detect_transitions
-from transitions import ColWipe, HorWipe, Cut
 import warnings
 
 def main():
@@ -31,9 +30,9 @@ def main():
             print("Resizing video to height=" + str(args.filename))
             print("Using histogram intersection threshold of: " + str(args.threshold))
             if args.ibm:
-                print("Using ibm colour histogram difference method")
+                print("Using ibm colour histogram difference method.")
             else:
-                print("Using histogram intersection method")
+                print("Using histogram intersection method.")
             print("This may take a while please be patient...")
         start = time()
         colsti, rowsti = breakdowntoSTI(args.filename, args.size, args.threshold, args.ibm)
@@ -50,7 +49,7 @@ def main():
     if args.verbose:
         print("Beginning transition detection.")
     start = time()
-    transitions = detect_transitions(colsti, rowsti)
+    transitions = detect_transitions(colsti, rowsti, args.hough)
     diff = time() - start
     if args.verbose:
         print("Completed in " + str(diff) + " seconds.")
@@ -62,8 +61,9 @@ def main():
     else:
         if args.verbose:
             print("Beginning video enhancement.")
+            print("This may take a while please be patient...")
         start = time()
-        enhance(args.filename, transitions, args.output)
+        enhance(args.filename, transitions, args.output, args.resolution)
         diff = time() - start
         if args.verbose:
             print("Completed in " + str(diff) + " seconds.")
@@ -73,14 +73,13 @@ def main():
         print("Program completed in " + str(diff) + " seconds.")
 
 
-
 def parse_args():
     parser = argparse.ArgumentParser(description='Enhance Transitions in Video.')
-    parser.add_argument("filename", help="Path to file that you would like to analyze") # main filename
+    parser.add_argument("filename", help="Path to file that you would like to analyze")  # main filename
     parser.add_argument("-v", "--verbose", action="store_true", help="Be verbose when executing")
-    parser.add_argument("-o", "--output", type=str, help="file to output to") # output file
+    parser.add_argument("-o", "--output", type=str, help="file to output to")  # output file
     parser.add_argument("-rg", "--regenerate", action="store_true", help="regenerate sti's even if they are found in "
-                                                                          "the path")
+                                                                         "the path")
     parser.add_argument("-w", "--write", action="store_true", help="Write STIs to improve speed for re-enhancing "
                                                                    "video with different options")
     parser.add_argument("-s", "--size", type=int, choices=[32, 64, 128, 256, 512], default=64, help="height video "
@@ -91,9 +90,13 @@ def parse_args():
                                                                                                     "higher may "
                                                                                                     "provide better "
                                                                                                     "quality")
+    parser.add_argument("-r", "--resolution", type=int, choices=[144, 240, 360, 480, 720, 900, 1080, 1440], default=720,
+                        help="output resolution of enhanced video, (will only scale down not up)")
     parser.add_argument("-t", "--threshold", type=float, default=0.7, help="threshold for which the intersection "
                                                                            "between consecutive colour histograms is "
                                                                            "considered abnormally low")
+    parser.add_argument("-l", "--hough", type=int, default=40, help="hough-line threshold used to for line detection "
+                                                                   "values between 0 and 100")
     parser.add_argument("-i", "--ibm", action="store_true", help="use ibm histogram difference method instead of "
                                                                  "histogram intersection method")
     parser.add_argument("-p", "--prominence", type=int, choices=range(1, 11), help="a number between 1 and 10 "
@@ -106,6 +109,10 @@ def parse_args():
     if args.threshold < 0.4 or args.threshold > 0.9:
         print("[threshold] must be between 0.4 and 0.9, continuing with threshold = 0.7")
         args.threshold = 0.7
+
+    if args.hough < 0 or args.hough > 100:
+        print("[hough] must be between 0 and 100, continuing with hough-line threshold = 40")
+        args.hough = 40
 
     # assign default output
     if args.output is None:
